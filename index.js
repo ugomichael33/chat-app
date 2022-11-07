9c1359482893abb00a50c8dc39a59a94834a7253
ugochukwu onyekanma <ugochikanele@gmail.com>
2022-11-07 23:36:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server')
+const gql = require('graphql-tag')
+
+const typeDefs = gql`
+    type Query{
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen({port: 5000})
+    .then(res => {
+        console.log(`server running at ${res.url}`);
+    }).catch(err => {
+        console.log(err);
+    })
